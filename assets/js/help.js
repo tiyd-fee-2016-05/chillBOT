e822c008f784e6bec65a5bcbfa599a6fd6f39340
Aaron <newcomer.aaron@icloud.com>
2016-06-20 14:32:09 -0400

    added submit function

--- assets/js/help.js
+++ assets/js/help.js
@@ -3,6 +3,13 @@ var chatBox= $(".chat-box");
 //var input = $('.write-here');
 
 // function to desplay help options
+$(".write-here").keypress(function (e) {  //Got this from stackoverflow, allows the enter key to work as a submit but not shift enter. Stackoverflow is bae
+    if(e.which == 13 && !e.shiftKey) {    //http://stackoverflow.com/questions/8934088/how-to-make-enter-key-in-a-textarea-submit-a-form
+        $(this).closest("form").submit();
+        e.preventDefault();
+        return false;
+    }
+});
 submit.submit(function (e){
   e.preventDefault()
   console.log("Submit works!");
