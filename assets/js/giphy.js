6225a78131da08389624b79359927a1c10ba2954
Aaron <newcomer.aaron@icloud.com>
2016-06-20 17:14:51 -0400

    removed a weird comment

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -6,7 +6,6 @@ $.ajax({
   url: "http://api.giphy.com/v1/gifs/search?q=" + giphySearch + "&api_key=dc6zaTOxFJmzC",
   method: "GET",
 }).done(function(json) {
-  // for (var x = 0; x< 10; x++)
     chatBox.append(
                         //'<li>' + json.data[0].images.downsized.url + '</li>'
                       '<div class=response-chat>' + "<img src='" + json.data[0].images.fixed_height.url + "'/>" + '</div>'
