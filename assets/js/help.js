753a3fba1655f19eff93feb19607574aaa8c79e3
Aaron <newcomer.aaron@icloud.com>
2016-06-20 15:21:14 -0400

    added auto scroll feature

--- assets/js/help.js
+++ assets/js/help.js
@@ -26,8 +26,11 @@ submit.submit(function (e){
   );
   }
   else if ($('.write-here').val() == "help") {
-    chatBox.append('<div class="response-chat">' + "Did you mean @Help?" + '</div>')
+    chatBox.append('<div class="response-chat">' + "Did you mean @help?" + '</div>')
   }
   //chatBox.first().css("display", "none");
   $(".write-here").val("");
+  $(".chat-box").animate({ scrollTop: $(".chat-box")[0].scrollHeight}, 500); //got this from Stackoverflow once again you are bae!
+                                                                            //http://stackoverflow.com/questions/14918787/jquery-scroll-to-bottom-of-div-even-after-it-updates
+
 })
