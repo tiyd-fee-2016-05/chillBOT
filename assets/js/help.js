df78adb96af0e5d8d3e31f2a077ad6e603fa6832
Aaron <newcomer.aaron@icloud.com>
2016-06-20 14:01:00 -0400

    Added bug fix to @help

--- assets/js/help.js
+++ assets/js/help.js
@@ -1,15 +1,20 @@
 var submit = $(".user-input");
 var chatBox= $(".chat-box");
-var input = $('textarea[name="user-input"]')
+//var input = $('.write-here');
 
 // function to desplay help options
-submit.click(function ({
-  if (input.val() === "@help"){
+submit.submit(function (e){
+  e.preventDefault()
+  console.log("Submit works!");
+  //$(".write-here").val("");
+
+  if ($('.write-here').val() == "@help"){
     chatBox.append('<div class="response-chat">' + '<p>Here are a list of commands<p></br>' +
     '<p>@temp city st gives you the current temperature for a city.</p></br>' +
     '<p>@giphy anything gives you a chill gif matchings your word </p></br>' +
     '<p>@chill genre gives you a chill movie to chill to</p></br>' +
     '<p> or...we can just chill?</p>'
-    )
+  );
   }
-}))
+  $(".write-here").val("");
+})
